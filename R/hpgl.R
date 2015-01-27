192af4b3cbb8859b5d16e17789b7e46cb4a7ec6e
Thomas Levine <_@thomaslevine.com>
2015-01-27 21:24:26 +0000

    add R

---
+++ R/hpgl.R
@@ -0,0 +1,14 @@
+devSVG <- function (file = "Rplots.hpgl", width = 10, height = 8,
+                    bg = "white", fg = "black", onefile = TRUE, xmlHeader = TRUE)
+{
+  dev <- .C("do_HPGL", as.character(file),
+            as.character(bg),as.character(fg),
+            as.double(width),as.double(height),
+            as.logical(FALSE), as.logical(xmlHeader),
+            as.logical(onefile),PACKAGE="RSvgDevice")
+
+  invisible(dev)
+}
+
+.onLoad <- function(libname, pkgname)
+  library.dynam("RHPGL", pkgname, libname)
