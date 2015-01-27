0d92ce55c901b84880fe6e7feb8ef5987cefbad0
Thomas Levine <_@thomaslevine.com>
2015-01-27 22:54:44 +0000

    case to match package

--- R/hpgl.R
+++ R/hpgl.R
@@ -5,10 +5,10 @@ devHPGL <- function (file = "Rplots.hpgl", width = 10, height = 8,
             as.character(bg),as.character(fg),
             as.double(width),as.double(height),
             as.logical(FALSE), as.logical(xmlHeader),
-            as.logical(onefile),PACKAGE="RHPGL")
+            as.logical(onefile),PACKAGE="rhpgl")
 
   invisible(dev)
 }
 
 .onLoad <- function(libname, pkgname)
-  library.dynam("RHPGL", pkgname, libname)
+  library.dynam("rhpgl", pkgname, libname)
