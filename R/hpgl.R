cf9cf797ab4615bc23b429adc6bf20870a5f3be1
Thomas Levine <_@thomaslevine.com>
2016-02-24 11:48:27 +0000

    try fixing ipr

--- R/hpgl.R
+++ R/hpgl.R
@@ -1,16 +1,15 @@
 hpgl <- function (file = "Rplots.hpgl", width = 10, height = 8,
                   bg = "white", fg = "black",
-                  ip = c(0, 8.5, 11)) {
-                 #units = c('mm', 'in', 'plotter-units')) {
-  plotter.units <- 1
-  millimeters <- 1/40
-  inches <- 1/1016
+                  ip = c(0, 8.5, 11),
+                  units = c('mm', 'in', 'plotter-units')) {
+  ipr <- c('plotter-units'=1, mm=1/40, 'in'=1/1016)[units[1]]
+                   
   ip <- paste0('IP', paste(ip, sep = ','), ';')
   sc <- paste0('SC', paste(c(0, width, height), sep = ','), ';')
   dev <- .C("do_HPGL", as.character(file),
             as.character(bg),as.character(fg),
             as.double(width),as.double(height),
-            as.double(mm),
+            as.double(ipr),
             as.character(ip), as.character(sc),
             PACKAGE="rhpgl")
 
